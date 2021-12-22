00555888e7424c38458caab1a18d8c8b459b41cd
raywang999 <root@STU-18147702157.localdomain>
2021-12-22 10:43:45 -0500

    refactor file-upload resolvers

---
+++ graphql/resolvers/upload.js
@@ -0,0 +1,29 @@
+const storeUpload = require('./helpers/storeUpload');
+const File = require('../../models/file');
+
+module.exports = {
+	Query: {
+		// Retrieve metadata of all files in our MongoDB database.
+		uploads: async () => {
+			return (await File.find()).map((file) => file._doc);
+		},
+	},
+	Mutation: {
+		// Store a single file
+		singleUpload: async (parent, args) => {
+			return storeUpload(args.file);
+		},
+		// Store multiple files
+		multipleUpload: async (parent, { files }) => {
+			if (!files) files = []; // Turn files into an empty list if it's undefined or null
+			// Ensure an error storing one upload doesn’t prevent storing the rest.
+			const results = await Promise.allSettled(files.map(storeUpload));
+			return results.reduce((storedFiles, { value, reason }) => {
+				if (value) storedFiles.push(value);
+				// Realistically you would do more than just log an error.
+				else console.error(`Failed to store upload: ${reason}`);
+				return storedFiles;
+			}, []);
+		},
+	},
+};
