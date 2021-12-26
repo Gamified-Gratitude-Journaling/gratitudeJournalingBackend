c823589ea16d2fe48d43d9012fc9df92b3fb7cde
raywang999 <root@STU-18147702157.localdomain>
2021-12-25 23:04:24 -0500

    Bug Fixes

--- graphql/resolvers/leaderboard.js
+++ graphql/resolvers/leaderboard.js
@@ -20,16 +20,12 @@ module.exports = {
 					if (a[1] < b[1]) return 1;
 					return 0;
 				});
-				console.log(users);
 				return await Promise.all(users.map(async ([userId, points]) => {
 					return {user: await merge.user(userId), points}; 
 				}));
 			} catch (err) {
 				throw (err);
 			}
-		},
-		journalEntryUploads: async (parent, args, context) => {
-			return (await JournalEntry.find({ user: context.userId })).map(entry => merge.transformJournalEntry(entry))
 		},
 	},
 	Mutation: {
